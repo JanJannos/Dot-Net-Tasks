d9db1ca398b9e4bb1e21cd41de87c91a06af11af
unknown <hanohj@gmail.com>
2019-09-01 12:35:42 +0300

    First Commit

---
+++ TasksPractice/Tasks-JAN/Task1-CreateTasks.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TasksPractice
+{
+
+    // Creating and testing tasks
+
+    internal class Program
+    {
+        public static void Write(char c)
+        {
+
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(c);
+            }
+        }
+
+        public static void Write(object oo)
+        {
+            int i = 1000;
+            while (i-- > 0)
+            {
+                Console.Write(oo);
+            }
+        }
+
+        public static int TextLength(object oo)
+        {
+            Console.WriteLine($"\nTask with id {Task.CurrentId} is processing object {oo}");
+            return oo.ToString().Length;
+        }
+
+        static void Main1(string[] args)
+        {
+            //Task.Factory.StartNew(() => Write('.'));
+            //var t = new Task(() => Write('?')); // 
+            //t.Start();
+            //Write('-');
+            //Console.WriteLine("Hello World!");
+
+            //Task t = new Task(Write , "hello");
+            //t.Start();
+            //Task.Factory.StartNew(Write, 123);
+
+            string text1 = "texting", text2 = "this";
+            var task1 = new Task<int>(TextLength , text1);
+            task1.Start();
+            Func<object, int> textLength = TextLength;
+            Task<int> task2 = Task.Factory.StartNew<int>(textLength, text2); 
+
+            // waiting for both tasks 
+
+            Console.WriteLine($"The length of '{text1}' is {task1.Result}");
+            Console.WriteLine($"The length of '{text2}' is {task2.Result}");
+
+
+            Console.WriteLine("Main Program Done !");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TasksPractice/Tasks-JAN/Task2-CancelingTasks.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TasksPractice.Tasks_JAN
+{
+    internal class Task2_CancelingTasks
+    {
+        // First part of the lesson
+        static void Main1(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var token = cts.Token;
+
+            // Register to an event when the user clicked something (First way , there's another below {marked XXXX})
+            token.Register(() =>
+            {
+                Console.WriteLine("Cancellation Has Been Requested!");
+
+            });
+
+
+            var t = new Task(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    //if (token.IsCancellationRequested)
+                    //{
+                    //    // one way : 
+                    //    //  break;
+
+                    //    // another way
+                    //    throw new OperationCanceledException();
+                    //}
+                    //else
+                    //{
+                    //    Console.WriteLine($"{i++}\t");
+                    //}
+
+                    // a third way is combining the two above ...
+                    token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\t");
+                }
+            }, token);
+
+            t.Start();
+
+
+            // {XXXX} Another way to know when the user clicked something 
+            Task.Factory.StartNew(() =>
+            {
+                token.WaitHandle.WaitOne();
+                Console.WriteLine("Wait handle released , cancellation has been requested ...");
+            });
+
+
+            Console.ReadKey();
+            cts.Cancel();
+
+
+            Console.WriteLine("Main program is Done!");
+            Console.ReadKey();
+        }
+
+
+        // Second part of the lesson
+        static void Main(string[] args)
+        {
+            var planned = new CancellationTokenSource();
+            var preventative = new CancellationTokenSource();
+            var emergency = new CancellationTokenSource();
+
+            var paranoid = CancellationTokenSource.CreateLinkedTokenSource(planned.Token,
+                preventative.Token, emergency.Token);
+
+            Task.Factory.StartNew(() =>
+            {
+                int i = 0;
+                while (true)
+                {
+                    paranoid.Token.ThrowIfCancellationRequested();
+                    Console.WriteLine($"{i++}\t");
+                    Thread.Sleep(100);
+                }
+            }, paranoid.Token);
+
+            Console.ReadKey();
+            emergency.Cancel();
+
+
+            Console.WriteLine("Main program is Done!");
+            Console.ReadKey();
+        }
+    }
+}
